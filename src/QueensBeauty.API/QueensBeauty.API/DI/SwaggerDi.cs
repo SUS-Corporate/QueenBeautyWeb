6ddff88b669112dae48f627a13e0cd891bef9f8c
Sasa Office <sasakiticsanz@gmail.com>
2023-03-15 14:55:33 +0100

    initial commit

---
+++ src/QueensBeauty.API/QueensBeauty.API/DI/SwaggerDi.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace QueensBeauty.API.DI
+{
+    public static class SwaggerDi
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "QueensBeauty.API",
+                    Version = "v1",
+                    Description = "Microservices HTTP API."
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+               // var xmlFile
+            });
+
+
+
+            return services;
+        }
+    }
+}
---
+++ src/QueensBeauty.API/QueensBeauty.API/Program.cs
@@ -0,0 +1,33 @@
+using QueensBeauty.API.DI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Here to add services custom
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwagger();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
